af06a7bae28e38786abb1dbf75d2481172863477
Will Teixeira <willrteixeira@gmail.com>
2018-01-26 16:07:28 -0500

    more spaceship, and a dead spaceman

--- source/GGJ2018_src/Assets/Scripts/PlayerShip.cs
+++ source/GGJ2018_src/Assets/Scripts/PlayerShip.cs
@@ -16,16 +16,45 @@ public class PlayerShip : MonoBehaviour
 
     public UnityEvent onFuelEmpty;
 
+    public bool canGo = false;
+
+    public GameObject explosionPrefab;
+    public GameObject deadSpacemanPrefab;
+
+    public GameObject model;
+
     // Use this for initialization
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        body.simulated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateFuel();
+        if (canGo)
+        {
+            UpdateFuel();
+        }
+    }
+
+    [ContextMenu("Reset Ship")]
+    public void ResetShip()
+    {
+        // TODO: Place me on the launchpad
+
+        model.SetActive(true);
+        ResetFuel();
+        body.simulated = false;
+    }
+
+    [ContextMenu("LAUNCH")]
+    public void LaunchShip()
+    {
+        canGo = true;
+        body.simulated = true;
+        // todo: play some launch sfx
     }
 
     public void ResetFuel()
@@ -56,6 +85,12 @@ public class PlayerShip : MonoBehaviour
 
     public void FixedUpdate()
     {
+        if(!canGo)
+        {
+            // make sure our gravity doesn't move us
+            //body.AddForce(Physics.gravity * -1f * body.gravityScale);
+            return;
+        }
         if (fuel > 0f)
         {
             body.AddForce(transform.up * forwardSpeed * Time.deltaTime, ForceMode2D.Force);
@@ -64,8 +99,42 @@ public class PlayerShip : MonoBehaviour
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        // TODO: YOU LOOSE!!
+        if (canGo)
+        {
+            WinPortal winPortal = collision.otherCollider.GetComponent<WinPortal>();
+            if(winPortal)
+            {
+                // YOU WIN!!!!
+                Debug.Log("You win!!!!");
 
 
+                gameObject.SetActive(false);
+                return;
+            }
+            // TODO: YOU LOOSE!!
+
+            // create our explosion and our dead space man
+            if (explosionPrefab)
+            {
+                GameObject goExplosion = GameObject.Instantiate<GameObject>(explosionPrefab, transform.position, Quaternion.identity, null);
+            }
+
+            if (deadSpacemanPrefab)
+            {
+                GameObject goSpaceman = GameObject.Instantiate<GameObject>(deadSpacemanPrefab, transform.position, Quaternion.identity, null);
+                // add a tiiiny amount of force to him, so he'll float
+                Rigidbody2D spacemanBody = goSpaceman.GetComponent<Rigidbody2D>();
+                if(spacemanBody)
+                {
+                    float forcePower = Random.Range(.1f, 1f);
+                    spacemanBody.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)), ForceMode2D.Impulse);
+                }
+            }
+
+            // hide our model
+            model.SetActive(false);
+            canGo = false;
+            body.simulated = false;
+        }
     }
 }
