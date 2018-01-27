f94ffc0c3b246eef6504378bfc593cd99c5b9ef5
BewareOfNargles <kimferguson13@gmail.com>
2018-01-26 20:27:19 -0500

    Item dragging script stub, initial fuel prefab.

---
+++ source/GGJ2018_src/Assets/Scripts/Input/ItemGrabManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGrabManager : MonoBehaviour
+{
+	private GameObject heldItem = null;
+
+	public GameObject HeldItem
+	{
+		get { return heldItem; }
+	}
+
+	#region Unity Messages
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if( heldItem != null )
+		{
+			//Vector2 mousePos = Input.mousePosition;
+			//Vector3 newPosition;
+		}
+	}
+	#endregion Unity Messages
+
+	#region Public Interface
+	public void GrabItem( GameObject item )
+	{
+
+	}
+	#endregion Public Interface
+
+	#region Helpers
+	private void ReleaseItem()
+	{
+
+	}
+	#endregion Helpers
+}
