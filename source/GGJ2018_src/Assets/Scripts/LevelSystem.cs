efe072e27dfacd03fc83a750ab8e62141ce46e61
Will Teixeira <willrteixeira@gmail.com>
2018-01-27 05:13:36 -0500

    Merge branch 'master' of https://github.com/theonlywill/GGJ2018
    level select, main menu, level editing tool (tools/level )

---
+++ source/GGJ2018_src/Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSystem
+{
+    public static LevelInfo currentLevel;
+
+
+    public static void LoadLevel(LevelInfo i_level)
+    {
+        currentLevel = i_level;
+        SceneManager.LoadScene(i_level.levelNumber.ToString());
+    }
+
+    public static int GetNumStars(int i_level)
+    {
+        return 0;
+    }
+}
