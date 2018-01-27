4f34991586d96517ecbdbcb034e792146a228e43
Rafael Lee Munoz <rafaelleemunoz@gmail.com>
2018-01-27 16:31:35 -0500

    Text
    Tutorial And dialogue

--- source/GGJ2018_src/Assets/Scripts/Editor/LevelEditWindow.cs
+++ source/GGJ2018_src/Assets/Scripts/Editor/LevelEditWindow.cs
@@ -14,19 +14,36 @@ public class LevelEditWindow : EditorWindow
     {
         scroll = EditorGUILayout.BeginScrollView(scroll);
 
-        LevelInfo[] levels = Resources.LoadAll<LevelInfo>("Levels");
-        for(int i = 0; i < levels.Length; i++)
+        List<LevelInfo> levels = new List<LevelInfo>();
+        levels.AddRange(Resources.LoadAll<LevelInfo>("Levels"));
+
+        levels.Sort(SortByName);
+
+        for(int i = 0; i < levels.Count; i++)
         {
             LevelInfo level = levels[i];
             EditorGUILayout.BeginHorizontal();
-            level.levelNumber = EditorGUILayout.IntField("Level", level.levelNumber);
+            //EditorGUILayout.ObjectField(level);
+            level.levelNumber = EditorGUILayout.IntField(level.name +":", level.levelNumber);
+            level.numAttract = EditorGUILayout.IntField("Attract:", level.numAttract);
+            level.numFuelPacks = EditorGUILayout.IntField("Fuel:", level.numAttract);
+            level.numRepel = EditorGUILayout.IntField("Repel:", level.numAttract);
+            level.numDelay = EditorGUILayout.IntField("Delay:", level.numAttract);
+            level.numShield = EditorGUILayout.IntField("Shield:", level.numAttract);
+            level.numMissile = EditorGUILayout.IntField("Missile:", level.numAttract);
             EditorGUILayout.EndHorizontal();
 
         }
 
+        
+
         EditorGUILayout.EndScrollView();
     }
     
+    private static int SortByName(LevelInfo a, LevelInfo b)
+    {
+        return a.levelNumber.CompareTo(b.levelNumber);
+    }
 
     /// <summary>
     /// Retrives the TransformUtilities window or creates a new one
--- source/GGJ2018_src/Assets/Scripts/UI/LevelSelect.cs
+++ source/GGJ2018_src/Assets/Scripts/UI/LevelSelect.cs
@@ -6,6 +6,7 @@ public class LevelSelect : MonoBehaviour
 {
 
     public GameObject levelButtonTemplate;
+    public GameObject buttonList;
 
 	// Use this for initialization
 	void Start ()
@@ -15,11 +16,39 @@ public class LevelSelect : MonoBehaviour
 
     void BuildLevelList()
     {
+        //hide our template
+        levelButtonTemplate.SetActive(false);
+        List<LevelInfo> levels = new List<LevelInfo>( Resources.LoadAll<LevelInfo>("Levels"));
+        levels.Sort(SortByNumber);
 
+        for (int i = 0; i < levels.Count; i++)
+        {
+            BuildLevelButton(levels[i]);
+        }
+    }
+
+    private static int SortByNumber(LevelInfo a, LevelInfo b)
+    {
+        return a.levelNumber.CompareTo(b.levelNumber);
+    }
+
+    void BuildLevelButton(LevelInfo i_level)
+    {
+        GameObject goNewButton = GameObject.Instantiate<GameObject>(levelButtonTemplate, buttonList.transform, false);
+        goNewButton.SetActive(true);
+        LevelButton button = goNewButton.GetComponent<LevelButton>();
+        button.levelInfo = i_level;
+        button.label.text = i_level.levelNumber.ToString();
+        button.UpdateStars();
     }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void HandleBackButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
 }
