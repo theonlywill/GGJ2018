b79f5ddd62332c97d571c51245cac85ea390e66e
BewareOfNargles <kimferguson13@gmail.com>
2018-01-26 19:39:46 -0500

    Stuff.

---
+++ source/GGJ2018_src/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	#region Singleton Access
+	private static GameManager activeInstance = null;
+	public static GameManager Instance
+	{
+		get { return activeInstance; }
+	}
+	#endregion Singleton Access
+
+	private void Awake()
+	{
+		if(activeInstance != null)
+		{
+			Destroy( gameObject );
+			return;
+		}
+
+		activeInstance = this;
+	}
+}
