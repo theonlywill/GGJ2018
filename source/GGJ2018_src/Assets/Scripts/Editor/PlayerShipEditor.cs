47b3c3d8525eb44054ad64de3d87d47430806509
MSI\Kristy <kristymoniz731@gmail>
2018-01-27 11:37:52 -0500

    Re Add Scripts?

---
+++ source/GGJ2018_src/Assets/Scripts/Editor/PlayerShipEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PlayerShip))]
+public class PlayerShipEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("LAUNCH!!!"))
+        {
+            PlayerShip ship = (PlayerShip)target;
+            ship.LaunchShip();
+        }
+    }
+}
